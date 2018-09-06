7e80cb1870ec2db35cf9abcb007b970b5a5d1905
Ilya Groshev <groshev.i.d@gmail.com>
2018-09-06 22:48:42 +0300

    test scenario

---
+++ testscenario.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# using https://github.com/zekchan/eos-local-env-setup setup
+
+eosiocpp -o ./rps/rps.wasm ./rps/rps.cpp
+cleos create account eosio rps EOS88PUwFsK1wYAiQEqEhUAGwXyv1hCcj4XPuCkvEyJ6sbtsxELiF EOS88PUwFsK1wYAiQEqEhUAGwXyv1hCcj4XPuCkvEyJ6sbtsxELiF
+cleos set contract rps ./rps -p rps
+cleos push action eosio.token transfer '["bob", "rps", "1.0000 EOS", "create:"]' -p bob
+cleos push action eosio.token transfer '["alice", "rps", "1.0000 EOS", "join:0"]' -p alice
+cleos get table rps rps games
