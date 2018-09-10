280bc3641ecf696e63e98ce67cb73167091ac86b
Ilya Groshev <groshev.i.d@gmail.com>
2018-09-10 20:34:57 +0300

    some eos tests

--- tests/cases/simplegame.js
+++ tests/cases/simplegame.js
@@ -1,5 +1,11 @@
+import eos, { deployContract } from "../tools/eos";
+jest.setTimeout(99999999);
 describe('Simple game Alise and Bob', () => {
   test('Alise wins bob', async () => {
-    expect(4).toBe(4)
+    const looper = () => deployContract().then((name) => {
+      console.log(name);
+      return looper();
+    })
+    await looper();
   })
 })
--- tests/tools/eos.js
+++ tests/tools/eos.js
@@ -1,13 +1,50 @@
 import Eos from 'eosjs';
+import path from 'path';
+import fs from 'fs';
 
 const keys = [
-  '5HrCoHT55VdetZp1mdih9UJ4cUftd1JbGsTQTZU14VhdHWKy92B', // alice
+  '5HrCoHT55VdetZp1mdih9UJ4cUftd1JbGsTQTZU14VhdHWKy92B', // alice EOS79NjptwuiygNmWmGLXTHoK4eUsZZxWgprQZ3rgDdJvkbLFTkSA
   '5KGVkBbyrpDoZrF4RGAV48N5xKxYCcLBUuhZokbvg9czcqxTrPo', // bob
   '5JMcyQ1VeZUjJ5HgBHjUGHLh6pWdr1UbPSgLETimkXBFW3Trh5a', // eosio token
   '5JBgxSG8aSMWik8ZCey2trACqCCVLute7Y7k5Y1CYdJSo3CYvY9', // ilya
 ]
 const eos = Eos({
-  keyProvider: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',// private key
+  keyProvider: keys,// private key 
   httpEndpoint: 'http://127.0.0.1:8888',
-  chainId: chain.sys,
+  chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
 });
+
+export default eos;
+
+
+function getAccountName() {
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz123";
+  const length = 1 + Math.floor(Math.random() * 10)
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+const wasm = fs.readFileSync(path.resolve('../rps/rps.wasm'));
+const abi = fs.readFileSync(path.resolve('../rps/rps.abi'));
+
+export async function deployContract() {
+  const accountName = getAccountName();
+
+  await eos.transaction(tr => {
+    tr.newaccount({
+      creator: 'alice',
+      name: accountName,
+      owner: 'EOS79NjptwuiygNmWmGLXTHoK4eUsZZxWgprQZ3rgDdJvkbLFTkSA',
+      active: 'EOS79NjptwuiygNmWmGLXTHoK4eUsZZxWgprQZ3rgDdJvkbLFTkSA'
+    })
+  })
+  await eos.transaction(tr => {
+    tr.setcode(accountName, 0, 0, wasm)
+    tr.setabi(accountName, JSON.parse(abi))
+  })
+  const contract = await eos.contract(accountName)
+
+  return await eos.contract(accountName);
+}
